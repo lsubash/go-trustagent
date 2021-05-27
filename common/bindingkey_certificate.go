94409bd3681f2afcd015530abd418b2a0757d28d
Dhawale, Dinkar <dinkar.dhawale@intel.com>
2021-05-27 21:02:49 +0530

    fix bk read

--- common/bindingkey_certificate.go
+++ common/bindingkey_certificate.go
@@ -5,8 +5,6 @@
 package common
 
 import (
-	"encoding/pem"
-	"errors"
 	"github.com/intel-secl/intel-secl/v4/pkg/lib/common/log/message"
 	"intel/isecl/go-trust-agent/v4/constants"
 	"io/ioutil"
@@ -15,20 +13,6 @@ import (
 )
 
 func (handler *requestHandlerImpl) GetBindingCertificateDerBytes() ([]byte, error) {
-	bindingKeyBytes, err := getBindingKeyPem()
-	if err != nil {
-		return nil, err
-	}
-
-	bindingKeyDer, _ := pem.Decode(bindingKeyBytes)
-	if bindingKeyDer == nil {
-		return nil, errors.New("There was an error parsing the Binding Key's der bytes")
-	}
-
-	return bindingKeyDer.Bytes, nil
-}
-
-func getBindingKeyPem() ([]byte, error) {
 	if _, err := os.Stat(constants.BindingKeyCertificatePath); os.IsNotExist(err) {
 		log.WithError(err).Errorf("common/binding_key_certificate:getBindingKeyCertificate() %s - %s does not exist", message.AppRuntimeErr, constants.BindingKeyCertificatePath)
 		return nil, &EndpointError{Message: "Error processing request", StatusCode: http.StatusInternalServerError}
